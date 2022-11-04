e983e9874eade54cdfe993aa61267f55eb19e969
Samson063 <furthermaths078@gamil.com>
2022-11-04 14:17:38 +0100

    feat: Added Contact Button

--- src/components/Anchors.js
+++ src/components/Anchors.js
@@ -1,5 +1,6 @@
 import slack from '../assets/slack.png';
-import github from '../assets/github.png';
+import github from '../assets/github-icon.png';
+import { Link } from 'react-router-dom';
 
 const Anchors = () => {
   const twitter = 'https://twitter.com/anth_samson?t=QFoD8vPm_V258m2tLeBT9A&s=09';
@@ -9,33 +10,36 @@ const Anchors = () => {
   const zuriDesign = 'https://books.zuri.team/design-rules';
 
   return (
-    <main className='container'>
-      <div className='buttons'>
-        <a href={twitter} id='twitter'>
-          Twitter Link
-        </a>
-        <a href={zuriBtn} id='btn__zuri'>
-          Zuri Team
-        </a>
-        <a href={zuriBooks} id='books'>
-          Zuri Books
-        </a>
-        <a href={zuriBooks} id='book__python'>
-          Python Books
-        </a>
-        <a href={pitch} id='pitch'>
-          Background Check for Coders
-        </a>
-        <a href={zuriDesign} id='book__design'>
-          Design Books
-        </a>
-      </div>
-      <div className='icons'>
-        <img src={slack} alt='slack' />
-        <img src={github} alt='github' />
-      </div>
-    </main>
-  );
+      <main className='container'>
+        <div className='buttons'>
+          <a href={twitter} id='twitter'>
+            Twitter Link
+          </a>
+          <a href={zuriBtn} id='btn__zuri'>
+            Zuri Team
+          </a>
+          <a href={zuriBooks} id='books'>
+            Zuri Books
+          </a>
+          <a href={zuriBooks} id='book__python'>
+            Python Books
+          </a>
+          <a href={pitch} id='pitch'>
+            Background Check for Coders
+          </a>
+          <a href={zuriDesign} id='book__design'>
+            Design Books
+          </a>
+          <Link to='/contact' id='contact'>
+            Contact Us
+          </Link>
+        </div>
+        <div className='icons'>
+          <img src={slack} alt='slack' />
+          <img src={github} alt='github' />
+        </div>
+      </main>
+    );
 };
 
 export default Anchors;
